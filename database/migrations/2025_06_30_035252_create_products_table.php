0b6c80b0923eefe97b49418e8d7a04471a26cc2d
Nazmi17 <ramadaninazmi47@gmail.com>
2025-07-01 11:05:49 +0700

    penambahan kolom dan fillable pada model

--- database/migrations/2025_06_30_035252_create_products_table.php
+++ database/migrations/2025_06_30_035252_create_products_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->decimal('price', 13, 2);
+            $table->text('description');
+            $table->integer('stock');
+            $table->text('image_url');
             $table->timestamps();
         });
     }
