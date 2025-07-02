2b4d3e054af6119b12356d117e2a58bf4e55fcaf
Nazmi17 <ramadaninazmi47@gmail.com>
2025-07-02 10:12:51 +0700

    setup api, google-api-client, dan controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Google\Client;
+use Google\Service\Drive;
+use Google\Service\Drive\DriveFile;
+use Google\Service\Drive\Permission;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    
+}
