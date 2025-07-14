a3672b4b0d75ec81dea0ef17fcf8d53ee0600398
Nazmi17 <ramadaninazmi47@gmail.com>
2025-07-14 17:21:18 +0700

    pemindahan routes kedalam prefix untuk kepraktisan

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 // Route::post('/test', [ProductController::class, 'testMakePublic']);
 
-Route::get('/categories', [ProductController::class, 'showCategories']);
-Route::get('/categories/{categorySlug}', [ProductController::class, 'showByCategory']);
+Route::prefix('categories')->group(function () {
+    Route::get('/', [ProductController::class, 'showCategories']);
+    Route::get('/{categorySlug}', [ProductController::class, 'showByCategory']);
 
-Route::post('/categories/{categorySlug}', [ProductController::class, 'store']);
-Route::put('/categories/{categorySlug}/{productSlug}', [ProductController::class, 'update']);
-Route::delete('/categories/{categorySlug}/{productSlug}', [ProductController::class, 'destroy']);
+    Route::post('/{categorySlug}', [ProductController::class, 'store']);
+    Route::put('/{categorySlug}/{productSlug}', [ProductController::class, 'update']);
+    Route::delete('/{categorySlug}/{productSlug}', [ProductController::class, 'destroy']);
+});
